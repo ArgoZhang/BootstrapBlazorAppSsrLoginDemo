fcfa77d581eac8557d07a015b80c6321ff66617c
Argo-Clound <argo@163.com>
2021-01-29 23:26:46 +0800

    init repos

---
+++ BootstrapBlazorApp1/src/BootstrapBlazorApp1.Server/App.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazorApp1.Server
+{
+    public partial class App
+    {
+        [Inject]
+        private AuthenticationStateProvider AuthenticationProvider { get; set; }
+
+        [Inject]
+        private NavigationManager Navigator { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var state = await AuthenticationProvider.GetAuthenticationStateAsync();
+            if (!state.User.Identity.IsAuthenticated)
+            {
+                Navigator.NavigateTo("/Account/Login");
+            }
+            else
+            {
+                await base.OnInitializedAsync();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+        }
+    }
+}
---
+++ BootstrapBlazorApp1/src/BootstrapBlazorApp1.Server/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazorApp1.Server.Controllers
+{
+    /// <summary>
+    /// Account controller.
+    /// </summary>
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 系统登录方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Login the specified userName, password and remember.
+        /// </summary>
+        /// <returns>The login.</returns>
+        /// <param name="userName">User name.</param>
+        /// <param name="password">Password.</param>
+        [HttpPost]
+        public async Task<IActionResult> Login(string userName, string password)
+        {
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, userName));
+            await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+            return Redirect("/");
+        }
+
+        /// <summary>
+        /// Logout this instance.
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <returns>The logout.</returns>
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/Account/Login");
+        }
+    }
+}
